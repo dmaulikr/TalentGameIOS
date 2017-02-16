362da424f7919ce95167611c95806724ea061913
Rohit Dabra <rohitdabra@Rohits-MacBook-Pro.local>
2017-02-16 09:55:54 +0530

    Localiztion

--- TalentGame/LocalizableStrings.swift
+++ TalentGame/LocalizableStrings.swift
@@ -12,6 +12,9 @@ private class Localizator {
     
     static let sharedInstance = Localizator()
     
+    
+    
+    
     lazy var localizableDictionary: NSDictionary! = {
         if let path = Bundle.main.path(forResource: "Localizable", ofType: "plist") {
             return NSDictionary(contentsOfFile: path)
@@ -20,6 +23,16 @@ private class Localizator {
     }()
     
     func localize(string: String) -> String {
+        
+        let lang = UserPreferencesHelper.getUserDefaultString(key: Constants.LOCALE_LANG)
+        
+        localizableDictionary = {
+            if let path = Bundle.main.path(forResource: "Localizable", ofType: "plist",inDirectory: (lang + ".lproj"),forLocalization: lang) {
+                return NSDictionary(contentsOfFile: path)
+            }
+            fatalError("Localizable file NOT found")
+        }()
+        
         guard let localizedString = (localizableDictionary.value(forKey: string) as! NSDictionary).value(forKey : "value") as? String else {
             assertionFailure("Missing translation for: \(string)")
             return ""
